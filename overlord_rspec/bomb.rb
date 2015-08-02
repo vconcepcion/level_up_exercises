9a4013eb5cc91663ac6d2c075835260d16533087
Valerie Concepcion <vconcepcion@enova.com>
2015-08-02 17:46:58 -0500

    Overlord RSpec/TDD exercise

---
+++ overlord_rspec/bomb.rb
@@ -0,0 +1,76 @@
+class Bomb
+  attr_reader :status, :attempts_remaining
+
+  MAX_ALLOWED_ATTEMPTS = 3
+
+  def initialize(activation_code: '1234', deactivation_code: '0000')
+    @activation_code    = validate_code(activation_code)
+    @deactivation_code  = deactivation_code # TODO validate deactivation code
+    @attempts_remaining = MAX_ALLOWED_ATTEMPTS
+    # TODO set initial status to :inactive
+    # ...
+  end
+
+  def enter_code(code)
+    raise OutOfServiceError if out_of_service?
+    if active?
+      # TODO lower number of remaining attempts
+      # ...
+    end
+    if @attempts_remaining <= 0
+      # TODO call the 'explode' method
+      # ...
+    else
+      process_attempt(code)
+    end
+  end
+
+  def active?
+    @status == :active
+  end
+
+  def inactive?
+    @status == :inactive
+  end
+
+  def out_of_service?
+    @status == :out_of_service
+  end
+
+  private
+
+  def process_attempt(code)
+    case validate_code(code)
+    when @activation_code
+      activate
+    when @deactivation_code
+      deactivate
+    end
+  end
+
+  def activate
+    # TODO change status to active
+    # ...
+  end
+
+  def deactivate
+    @attempts_remaining = MAX_ALLOWED_ATTEMPTS
+    # TODO change status to inactive
+    # ...
+  end
+  alias_method :reset, :deactivate
+
+  def explode
+    @status = :out_of_service
+  end
+
+  def validate_code(code)
+    code.tap do |c|
+      c.gsub!(/\s/, '')
+      raise InvalidInputError, "Code '#{c}' must only include numeric characters." unless !!(/^\d+$/ =~ c)
+    end
+  end
+end
+
+class InvalidInputError < StandardError; end
+class OutOfServiceError < RuntimeError; end
---
+++ overlord_rspec/spec/bomb_spec.rb
@@ -0,0 +1,87 @@
+describe Bomb do
+  it 'is inactive when created', :practice => true, :exercise => 1 do
+    bomb = Bomb.new
+    expect(bomb.status).to eq(:inactive)
+  end
+
+  describe 'activation' do
+    it 'is not activated if the wrong activation code is entered' do
+      bomb = Bomb.new(activation_code: '1234')
+      bomb.enter_code('4321')
+      expect(bomb).to be_inactive
+    end
+
+    it 'is activated if the correct activation code is entered', :practice => true, :exercise => 2
+
+    it 'does nothing if the correct activation code is entered again', :practice => true, :exercise => 3
+
+  end
+
+  describe 'deactivation' do
+    let(:bomb) do
+      Bomb.new(activation_code:   '1111',
+               deactivation_code: '2222')
+    end
+
+    it 'is deactivated if the correct deactivation code is entered' do
+      bomb.enter_code('1111')
+      bomb.enter_code('2222')
+      expect(bomb).to be_inactive
+    end
+
+    it 'lowers the number of remaining attempts by 1 if the wrong deactivation code is entered', :practice => true, :exercise => 4
+
+    it 'does not explode if the wrong deactivation code is entered twice' do
+      bomb.enter_code('1111')
+      expect(bomb).not_to receive(:explode)
+      2.times { bomb.enter_code('6666') }
+    end
+
+    it 'explodes if the wrong deactivation code is entered three times', :practice => true, :exercise => 5
+
+    context 'when it has exploded' do
+      before do
+        # TODO make the bomb explode
+        # ...
+      end
+
+      it 'indicates that it is out-of-service', :practice => true, :exercise => 6 do
+        expect(bomb).to be_out_of_service
+      end
+
+      it 'does not accept any input' do
+        expect { bomb.enter_code('2222') }.to raise_error(OutOfServiceError)
+      end
+    end
+  end
+
+  describe 'default configuration' do
+    it "has a default activation code of '1234'" do
+      subject.enter_code('1234')
+      expect(subject).to be_active
+    end
+
+    it "has a default deactivation code of '0000'" do
+      subject.enter_code('1234')
+      subject.enter_code('0000')
+      expect(subject).to be_inactive
+    end
+  end
+
+  describe 'input validation' do
+    it "rejects the non-numeric input 'foo'", :practice => true, :exercise => 7 do
+      expect {
+        # TODO do something here that will raise an input error
+        # ...
+      }.to raise_error(InvalidInputError)
+    end
+
+    it "rejects the non-numeric input '1234!'", :practice => true, :exercise => 8
+
+    it 'validates the configured activation code' do
+      expect { Bomb.new(activation_code: 'foo') }.to raise_error(InvalidInputError)
+    end
+
+    it 'validates the configured deactivation code', :practice => true, :exercise => 9
+  end
+end
